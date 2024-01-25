4276583216e84fea89d4c88514c04be2d8043d8c
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-01-25 20:25:42 +0530

    Feat : Navbar ui fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,15 +5,16 @@ import { Menu, X } from "lucide-react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 const menuItems = [
   {
     name: "Home",
-    href: "#",
+    href: "/",
   },
   {
     name: "About",
-    href: "#",
+    href: "/",
   },
   {
     name: "List Event",
@@ -22,6 +23,8 @@ const menuItems = [
 ];
 
 export function Navbar() {
+  const pathName = usePathname();
+
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const toggleMenu = () => {
@@ -33,21 +36,11 @@ export function Navbar() {
       <MaxWidthWrapper>
         <div className="mx-auto flex items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
           <div className="inline-flex items-center space-x-2">
-            <span>
-              <svg
-                width="30"
-                height="30"
-                viewBox="0 0 50 56"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-                  fill="black"
-                />
-              </svg>
+            <span className="font-bold text-4xl">
+              <div className="bg-gradient-to-r from-purple-600 via-blue-500 to-blue-400 bg-clip-text text-transparent">
+                Eventify
+              </div>
             </span>
-            <span className="font-bold">DevUI</span>
           </div>
           <div className="hidden lg:block">
             <ul className="inline-flex space-x-8">
@@ -55,7 +48,10 @@ export function Navbar() {
                 <li key={item.name}>
                   <Link
                     href={item.href}
-                    className="text-sm font-semibold text-gray-800 hover:text-gray-900"
+                    className={`text-sm font-semibold ${pathName === item.href
+                      ? " border-b-2 border-blue-600 bg-gradient-to-r from-purple-600 via-blue-500 to-blue-400 bg-clip-text text-transparent"
+                      : "text-gray-800 hover:text-gray-900"
+                      }`}
                   >
                     {item.name}
                   </Link>
@@ -63,13 +59,20 @@ export function Navbar() {
               ))}
             </ul>
           </div>
-          <div className="hidden lg:block">
-            <Button
-              type="button"
-              // className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-            >
-              Login
-            </Button>
+          <div className="hidden lg:block ">
+            <Link href="/login">
+              <Button type="button" variant="ghost" className="mr-2 ">
+                Login
+              </Button>
+            </Link>
+            <Link href="/register">
+              <Button
+                type="button"
+                className="bg-gradient-to-r from-purple-600 via-blue-500 to-blue-400 p-4"
+              >
+                Register
+              </Button>
+            </Link>
           </div>
           <div className="lg:hidden">
             <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
@@ -80,21 +83,9 @@ export function Navbar() {
                 <div className="px-5 pb-6 pt-5">
                   <div className="flex items-center justify-between">
                     <div className="inline-flex items-center space-x-2">
-                      <span>
-                        <svg
-                          width="30"
-                          height="30"
-                          viewBox="0 0 50 56"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-                            fill="black"
-                          />
-                        </svg>
-                      </span>
-                      <span className="font-bold">DevUI</span>
+                      <div className="text-4xl font-bold bg-gradient-to-r from-purple-600 via-blue-500 to-blue-400 bg-clip-text text-transparent">
+                        Eventify
+                      </div>
                     </div>
                     <div className="-mr-2">
                       <button
@@ -110,24 +101,38 @@ export function Navbar() {
                   <div className="mt-6">
                     <nav className="grid gap-y-4">
                       {menuItems.map((item) => (
-                        <a
-                          key={item.name}
+                        <Link
                           href={item.href}
-                          className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50"
+                          key={item.name}
+                          className={`text-sm font-semibold ${pathName === item.href
+                            ? "bg-gradient-to-r from-purple-600 via-blue-500 to-blue-400 bg-clip-text text-transparent"
+                            : "text-gray-800 hover:text-gray-900"
+                            }`}
                         >
-                          <span className="ml-3 text-base font-medium text-gray-900">
-                            {item.name}
-                          </span>
-                        </a>
+                          {item.name}
+                        </Link>
                       ))}
                     </nav>
                   </div>
-                  <button
-                    type="button"
-                    className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                  >
-                    Button text
-                  </button>
+                  <div className="w-full my-2">
+                    <Link href="/login" className="w-full">
+                      <Button
+                        type="button"
+                        className="w-full mb-2"
+                        variant="outline"
+                      >
+                        Login
+                      </Button>
+                    </Link>
+                    <Link href="/register" className="w-full">
+                      <Button
+                        type="button"
+                        className="bg-gradient-to-r from-purple-600 via-blue-500 to-blue-400 w-full"
+                      >
+                        Register
+                      </Button>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
