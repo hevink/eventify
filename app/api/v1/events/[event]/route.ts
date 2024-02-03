a554a35b2520d47a41c3da394a9a8dea968150d5
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-02-03 18:44:56 +0530

    Fix: build error

--- app/api/v1/events/[event]/route.ts
+++ app/api/v1/events/[event]/route.ts
@@ -1,26 +1,6 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { event: string } }) {
-  console.log(params);
-
-  try {
-    const event = await db.event.findUnique({
-      where: {
-        id: params.event,
-      },
-    });
-    return NextResponse.json({
-      status: 200,
-      event,
-    });
-  } catch (error) {
-    return NextResponse.json({
-      error,
-    });
-  }
-}
-
 export async function POST(
   request: Request,
   { params }: { params: { event: string } }
