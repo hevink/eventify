811bc91911f27bdb6642614742e30497ad8857d2
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-02-06 20:35:11 +0530

    Feat : middleware changes

--- middleware.ts
+++ middleware.ts
@@ -14,13 +14,13 @@ export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
-  // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  // if (isApiAuthRoute) {
-  //   return null;
-  // }
+  if (isApiAuthRoute) {
+    return null;
+  }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
