3c62103bf842afb306a13b88dddcc54b88d3ea45
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-02-21 11:43:55 +0530

    Refactor useDebounce hook to use double quotes for import statement

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useDebouncedValue = <T>(value: T, delay: number): T => {
-    const [debouncedValue, setDebouncedValue] = useState(value);
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delay);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [value, delay]);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
 
-    return debouncedValue;
+  return debouncedValue;
 };
 
 export default useDebouncedValue;
